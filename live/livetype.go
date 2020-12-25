3970f2b89b53d1250cba059234f4802175c870d1
lidan <2364752564@qq.com>
2020-12-25 09:28:22 +0800

    feat : add  直播模板  直播类型  api

---
+++ live/livetype.go
@@ -0,0 +1,27 @@
+package live
+
+//LivetypeRespParam 直播类型响应参数
+type LivetypeRespParam struct {
+	Code    int      `json:"code"`    // 整型错误码，0成功，其它值失败
+	Message string   `json:"message"` // 字符串，可能包含错误或者别的一些信息
+	Success bool     `json:"success"` // true 或 false
+	Result  Livetype `json:"result"`  // 字符串，返回的结果
+
+}
+
+//LivetypeListRespParam 直播类型查询列表返回参数
+type LivetypeListRespParam struct {
+	Code    int        `json:"code"`    // 整型错误码，0成功，其它值失败
+	Message string     `json:"message"` // 字符串，可能包含错误或者别的一些信息
+	Success bool       `json:"success"` // true 或 false
+	Result  []Livetype `json:"result"`  // 字符串，返回的结果
+}
+
+//Livetype 直播类型
+type Livetype struct {
+	ID         string `json:"id,omitempty" `        //:唯一标识
+	Name       string `json:"name"`                 //模版名称/码率/宽x高
+	UserID     int64  `json:"userId,omitempty"`     //用户id
+	CreateTime int64  `json:"createTime,omitempty"` //创建时间
+	UpdateTime int64  `json:"updateTime,omitempty"` //更新时间
+}
--- tests/live_test.go
+++ tests/live_test.go
@@ -29,8 +29,8 @@ func TestCreatTemplate(t *testing.T) {
 	template := live.Template{}
 	template.DisplayName = fmt.Sprintf("TestCreatTemplate %d", rand.Intn(99999))
 	template.Name = fmt.Sprintf("TestCreatTemplate Name %d", rand.Intn(99999))
-	template.VideoWidth = rand.Intn(2048)
-	template.VideoHeight = rand.Intn(2048)
+	template.VideoWidth = rand.Intn(1000)
+	template.VideoHeight = rand.Intn(1000)
 	template.AudioBitrate = rand.Intn(1000)
 	template.FrameRate = rand.Intn(1000)
 
@@ -69,6 +69,182 @@ func TestGetTemplates(t *testing.T) {
 	} else if !templateRsp.Success {
 		t.Errorf("Success:%v, msg:%s", templateRsp.Success, templateRsp.Message)
 
+	}
+
+	fmt.Println(templateRsp)
+}
+
+func TestEditTemplate(t *testing.T) {
+	accessKey := "275ee3cd-6fb"
+	accessSecret := "5GcXHNYdAVVdFW0yervG"
+
+	if TestAccessKey != "" {
+		accessKey = TestAccessKey
+		accessSecret = TestSecretKey
+	}
+
+	fmt.Println(accessKey)
+	fmt.Println(accessSecret)
+
+	cred := auth.New(accessKey, accessSecret)
+
+	livemanager := live.NewLiveManager(cred)
+
+	template := live.Template{}
+	template.ID = "26c3ebf49cf64153a270ace17a074e58"
+	template.DisplayName = "修改直播模板名称"
+
+	templateRsp, err := livemanager.EditTemplate(template)
+
+	if err != nil {
+		//t.Error("sign error ")
+		t.Errorf(err.Error())
+	}
+
+	fmt.Println(templateRsp)
+}
+
+func TestDeleteTemplate(t *testing.T) {
+	accessKey := "275ee3cd-6fb"
+	accessSecret := "5GcXHNYdAVVdFW0yervG"
+
+	if TestAccessKey != "" {
+		accessKey = TestAccessKey
+		accessSecret = TestSecretKey
+	}
+
+	fmt.Println(accessKey)
+	fmt.Println(accessSecret)
+
+	cred := auth.New(accessKey, accessSecret)
+
+	livemanager := live.NewLiveManager(cred)
+
+	param := live.Template{ID: "26c3ebf49cf64153a270ace17a074e58"}
+
+	templateRsp, err := livemanager.DeleteTemplates(param)
+
+	if err != nil {
+		//t.Error("sign error ")
+		t.Errorf(err.Error())
+	}
+
+	fmt.Println(templateRsp)
+
+}
+
+func TestGetLivetypes(t *testing.T) {
+	accessKey := "275ee3cd-6fb"
+	accessSecret := "5GcXHNYdAVVdFW0yervG"
+
+	if TestAccessKey != "" {
+		accessKey = TestAccessKey
+		accessSecret = TestSecretKey
+	}
+
+	fmt.Println(accessKey)
+	fmt.Println(accessSecret)
+
+	cred := auth.New(accessKey, accessSecret)
+
+	livemanager := live.NewLiveManager(cred)
+
+	templateRsp, err := livemanager.GetLivetypes("", "3daa3949c294456ab40273ab2d8afba1")
+
+	if err != nil {
+		//t.Error("sign error ")
+		t.Errorf(err.Error())
+	} else if !templateRsp.Success {
+		t.Errorf("Success:%v, msg:%s", templateRsp.Success, templateRsp.Message)
+
+	}
+
+	fmt.Println(templateRsp)
+}
+
+func TestCreatLivetype(t *testing.T) {
+
+	accessKey := "275ee3cd-6fb"
+	accessSecret := "5GcXHNYdAVVdFW0yervG"
+
+	if TestAccessKey != "" {
+		accessKey = TestAccessKey
+		accessSecret = TestSecretKey
+	}
+
+	fmt.Println(accessKey)
+	fmt.Println(accessSecret)
+
+	cred := auth.New(accessKey, accessSecret)
+
+	livemanager := live.NewLiveManager(cred)
+
+	livetype := live.Livetype{}
+	livetype.Name = fmt.Sprintf("直播类型 %d", rand.Intn(99999))
+	templateRsp, err := livemanager.CreateLivetype(livetype)
+
+	if err != nil {
+		//t.Error("sign error ")
+		t.Errorf(err.Error())
+	}
+
+	fmt.Println(templateRsp)
+}
+
+func TestEditLivetype(t *testing.T) {
+
+	accessKey := "275ee3cd-6fb"
+	accessSecret := "5GcXHNYdAVVdFW0yervG"
+
+	if TestAccessKey != "" {
+		accessKey = TestAccessKey
+		accessSecret = TestSecretKey
+	}
+
+	fmt.Println(accessKey)
+	fmt.Println(accessSecret)
+
+	cred := auth.New(accessKey, accessSecret)
+
+	livemanager := live.NewLiveManager(cred)
+
+	livetype := live.Livetype{}
+	livetype.ID = "331f756633ce4ec887d1df021cc51b35"
+	livetype.Name = "修改直播类型--测试"
+	templateRsp, err := livemanager.EditLivetype(livetype)
+
+	if err != nil {
+		//t.Error("sign error ")
+		t.Errorf(err.Error())
+	}
+
+	fmt.Println(templateRsp)
+}
+
+func TestDeleteLivetype(t *testing.T) {
+
+	accessKey := "275ee3cd-6fb"
+	accessSecret := "5GcXHNYdAVVdFW0yervG"
+
+	if TestAccessKey != "" {
+		accessKey = TestAccessKey
+		accessSecret = TestSecretKey
+	}
+
+	fmt.Println(accessKey)
+	fmt.Println(accessSecret)
+
+	cred := auth.New(accessKey, accessSecret)
+
+	livemanager := live.NewLiveManager(cred)
+
+	livetype := live.Livetype{ID: "e402a628a14a4ce29de447741c3ba46d"}
+
+	templateRsp, err := livemanager.DeleteLivetype(livetype)
+
+	if err != nil {
+		//t.Error("sign error ")
+		t.Errorf(err.Error())
 	}
 
 	fmt.Println(templateRsp)
